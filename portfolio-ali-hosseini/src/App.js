fe42312c36232f8b2dcdbe256adb34ccfcb1e3ae
ahosseini06 <ali20060101@gmail.com>
2024-10-02 13:23:31 -0700

    scroll into view and structure change

--- portfolio-ali-hosseini/src/App.js
+++ portfolio-ali-hosseini/src/App.js
@@ -1,50 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-import Background from './components/background/Background';
-import Link from './components/link/Link';
-
-import { FaGithub } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { IoCamera } from "react-icons/io5";
-import { FaSquareInstagram } from "react-icons/fa6";
-import { IoIosArrowDown } from "react-icons/io";
-
-
-
-
-
+import React from 'react';
+import { useRef } from 'react';
+import Hero from './pages/Hero';
+import Projects from './pages/Projects';
 
 
 function App() {
-  const links = [
-    {name: "Github", link: "https://github.com/ahosseini06", icon:<FaGithub className='icon'/>},
-    {name: "Linked-In", link: "https://www.linkedin.com/in/01-ali-hosseini/", icon:<FaLinkedin className='icon'/>},
-    {name: "Instagram", link: "https://www.instagram.com/cxndymedia/", icon:<FaSquareInstagram className='icon'/>},
-    {name: "Media", link: "https://www.cxndy.ca", icon:<IoCamera className='icon'/>},
-  ]
+  const projectsRef = useRef(null);
+  const handleClick = () => {
+    projectsRef.current.scrollIntoView({ behavior: "smooth" });
+  };
   return (
-    <div className="App">
+    <div>
+      <Hero handleClick={handleClick}/>
+      <div ref={projectsRef}>
+        <Projects />
+      </div>
       
-        <div className="hero-container">
-          <h1>ALI HOSSEINI</h1>
-          <div className="link-container">
-            {links.map(link => {
-              return (
-                <Link name={link.name} link={link.link} icon={link.icon}>
-                </Link>
-              );
-            })}
-          </div>
-        </div>
-        <div style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-          <div className="view-btn">
-            View my work
-            <IoIosArrowDown className='arrow'/>
-          </div>
-        </div>
-          
-
     </div>
   );
 }
---
+++ portfolio-ali-hosseini/src/pages/Hero.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+import "./Hero.css";
+
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { IoCamera } from "react-icons/io5";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { IoIosArrowDown } from "react-icons/io";
+import Link from "../components/link/Link";
+
+const Hero = ({ handleClick }) => {
+  const links = [
+    {
+      name: "Github",
+      link: "https://github.com/ahosseini06",
+      icon: <FaGithub className="icon" />,
+    },
+    {
+      name: "Linked-In",
+      link: "https://www.linkedin.com/in/01-ali-hosseini/",
+      icon: <FaLinkedin className="icon" />,
+    },
+    {
+      name: "Instagram",
+      link: "https://www.instagram.com/cxndymedia/",
+      icon: <FaSquareInstagram className="icon" />,
+    },
+    {
+      name: "Media",
+      link: "https://www.cxndy.ca",
+      icon: <IoCamera className="icon" />,
+    },
+  ];
+  return (
+    <div className="hero">
+      <div className="hero-container">
+        <h1>ALI HOSSEINI</h1>
+        <div className="link-container">
+          {links.map((link) => {
+            return (
+              <Link name={link.name} link={link.link} icon={link.icon}></Link>
+            );
+          })}
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <button className="view-btn" href="#projects" onClick={handleClick}>
+          View my work
+          <IoIosArrowDown className="arrow" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
